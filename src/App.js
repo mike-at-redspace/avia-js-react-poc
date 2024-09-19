a0779d3288cf43e21501355f28e2e7d940a374a3
Mike Vardy <85122551+mike-at-redspace@users.noreply.github.com>
2024-09-19 08:17:41 -0400

    wip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Avia } from "@cbsinteractive/avia-js-react";
+import { useState, useRef } from "react";
 
 function App() {
+  const playerRef = useRef(null);
+  const [options] = useState({
+    autoplay: "attemptMuted",
+  });
+
+  const [resource] = useState({
+    location: {
+      mediaUrl:
+        "https://media.amlg.io/assets/splice_preview/2024/08/27/2344955459581/V-24-1590-06_QPDO_Trailer_1_FL_1920_x_1080_080524_FINAL_2780226_2780226_3133.mp4",
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="video-background" ref={playerRef}>
+      <Avia
+        options={options}
+        resource={resource}
+        onError={(error) => console.log(error)}
+        controls={false}
+        debug
+        dash={false}
+        hls={true}
+        fw={false}
+        gam={false}
+      ></Avia>
     </div>
   );
 }
